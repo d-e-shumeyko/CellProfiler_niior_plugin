a53a9dbfba2bd822b52596e7097c0fa9df89c5c6
d-e-shumeyko <d.e.shumeyko@gmail.com>
2025-08-26 12:35:50 +0300

    fixed version conflicts

--- CellProfiler-plugins-master/active_plugins/niiorMLMDatabasePlugin.py
+++ CellProfiler-plugins-master/active_plugins/niiorMLMDatabasePlugin.py
@@ -317,7 +317,7 @@ You have two options on how to resize your image:
 -  *{C_MANUAL}:* Specify the height and width of the output image.
 
             """.format(
-                **{ C_MANUAL}
+                **{"C_MANUAL":  C_MANUAL}
             ),
         )
 
@@ -482,6 +482,8 @@ resized with the same settings as the first image.""",
         exp_options = {
             "png": "png"
         }
+
+        self.apply_resize(workspace, input_objects_name, input_objects_name)
         
         filenames = saveresizedcroppedobjects(
             input_objects=input_objects,
@@ -710,39 +712,29 @@ def spline_order(self):
                 'interpolation error'
             )
 def apply_resize(self, workspace, input_image_name, output_image_name):
-        image = workspace.image_set.get_image(input_image_name)
+        objects = workspace.object_set.get_objects(self.objects_name.value)
 
-        image_pixels = image.pixel_data
+        image_pixels = objects.pixel_data
 
-        new_shape = self.resized_shape(image, workspace)
+        new_shape = self.resized_shape(objects, workspace)
 
         order = self.spline_order()
 
-        if image.volumetric and image.multichannel:
-            output_pixels = numpy.zeros(new_shape.astype(int), dtype=image_pixels.dtype)
-
-            for idx in range(int(new_shape[-1])):
-                output_pixels[:, :, :, idx] = skimage.transform.resize(
-                    image_pixels[:, :, :, idx],
-                    new_shape[:-1],
-                    order=order,
-                    mode="symmetric",
-                )
-        else:
-            output_pixels = skimage.transform.resize(
+        
+        output_pixels = skimage.transform.resize(
                 image_pixels, new_shape, order=order, mode="symmetric"
             )
 
-        if image.multichannel and len(new_shape) > image.dimensions:
+        if objects.multichannel and len(new_shape) > objects.dimensions:
             new_shape = new_shape[:-1]
 
-        mask = skimage.transform.resize(image.mask, new_shape, order=0, mode="constant")
+        mask = skimage.transform.resize(objects.mask, new_shape, order=0, mode="constant")
 
         mask = skimage.img_as_bool(mask)
 
-        if image.has_crop_mask:
+        if objects.has_crop_mask:
             cropping = skimage.transform.resize(
-                image.crop_mask, new_shape, order=0, mode="constant"
+                objects.crop_mask, new_shape, order=0, mode="constant"
             )
 
             cropping = skimage.img_as_bool(cropping)
@@ -751,25 +743,25 @@ def apply_resize(self, workspace, input_image_name, output_image_name):
 
         output_image = Image(
             output_pixels,
-            parent_image=image,
+            parent_image=objects,
             mask=mask,
             crop_mask=cropping,
-            dimensions=image.dimensions,
+            dimensions=objects.dimensions,
         )
 
         workspace.image_set.add(output_image_name, output_image)
 
         if self.show_window:
             if hasattr(workspace.display_data, "input_images"):
-                workspace.display_data.multichannel += [image.multichannel]
-                workspace.display_data.input_images += [image.pixel_data]
+                workspace.display_data.multichannel += [objects.multichannel]
+                workspace.display_data.input_images += [objects.pixel_data]
                 workspace.display_data.output_images += [output_image.pixel_data]
                 workspace.display_data.input_image_names += [input_image_name]
                 workspace.display_data.output_image_names += [output_image_name]
             else:
-                workspace.display_data.dimensions = image.dimensions
-                workspace.display_data.multichannel = [image.multichannel]
-                workspace.display_data.input_images = [image.pixel_data]
+                workspace.display_data.dimensions = objects.dimensions
+                workspace.display_data.multichannel = [objects.multichannel]
+                workspace.display_data.input_images = [objects.pixel_data]
                 workspace.display_data.output_images = [output_image.pixel_data]
                 workspace.display_data.input_image_names = [input_image_name]
                 workspace.display_data.output_image_names = [output_image_name]
